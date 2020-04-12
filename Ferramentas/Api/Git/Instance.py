945680464b0ff8dc4d286a00b99ea96ce77ea03c
Wesley Guedes <wguedes@licitacao.com.br>
2020-04-12 19:03:20 -0300

    Pequenos ajustes na dockerfile e nas classes dependentes de integração

--- Ferramentas/Api/Git/Instance.py
+++ Ferramentas/Api/Git/Instance.py
@@ -2,45 +2,29 @@ from github import Github
 from github import GitRelease
 
 
-class Git():
-    TOKKEN = ''
+class Git:
+    GitToken = ''
     hostName = ''
-    git = Github(TOKKEN)
-
-    def getYourRepository(self):
-        """Pegar os seus repositórios
-                            Github Enterprise with custom hostname
-                            """
-
-        self.git = Github(base_url=f"https://{self.hostName}/api/v3", login_or_token=self.TOKKEN)
+    # Coleta de repositório do github (só Enterprise?)
+    git = Github(base_url=f"https://{hostName}/api/v3", login_or_token=GitToken)
 
     def getRepository(self):
-        """Pegar repositórios pesquisando via  query"""
+        # Pegar repositórios pesquisando via query
         repositories = self.git.search_repositories(query='language:python')
         for repo in repositories:
             print(repo)
 
     def getBranch(self, query):
-        '''Função destinada para retornar a lista de branchs
-
-         Passar a query, exemplo: "PyGithub/PyGithub"
-
-        Ele retornará uma lista
-
-        '''
+        #Função destinada para retornar a lista de branches
+        #Passar a query, exemplo: "PyGithub/PyGithub"
+        #Ele retornará uma lista
         repo = self.git.get_repo(query)
         lista = list(repo.get_branches())
-
         return lista
 
     def getReleases(self, query):
-        '''Função destinada para retornar os realeses e retornando uma lista'''
+        #Função destinada para retornar os releases e retornando uma lista
         repo = self.git.get_repo(query)
         lista = list(repo.get_releases())
 
         return lista
-
-
-
-
-
