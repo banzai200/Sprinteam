be194b1a4ee087045987dcb596da10d83296652b
Aurélio Souza <aurelio2010ss@gmail.com>
2020-04-16 20:55:06 -0300

    Criada visualização de detalhes de cards, feitas alterações no css e implementação de js para elementos dinâmicos.

---
+++ kanban/static/js/mainscripts.js
@@ -0,0 +1,8 @@
+function showCard(){
+    var card=document.getElementById("cardDetail");
+    if(card.style.display=== "none"){
+        card.style.display="block";
+    } else{
+        card.style.display="none";
+    }
+} 
