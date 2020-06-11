570b325d89ad7e4d0fc70460aee413cab4aba501
Wesley Guedes <tairoria@gmail.com>
2020-06-11 14:02:24 -0300

    Goddammit

--- kanban/forms.py
+++ kanban/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Cards
 
 
 class SignUpForm(UserCreationForm):
@@ -13,9 +15,7 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
 
-class CardForm(forms.Form):
-    card_name = forms.CharField(max_length=200, label='Nome do Card')
-    card_complexity = forms.IntegerField(max_value=100, label='Complexidade do Card')
-    card_topic = forms.CharField(max_length=10, label='Tópico do Card')
-    card_deadline = forms.DateField(label='Data limite para entrega')
-    card_description = forms.CharField(max_length=500, label="Descrição do Card")
+class CardForm(ModelForm):
+    class Meta:
+        model = Cards
+        fields = ('c_name', 'c_complexity', 'c_topic', 'c_deadline', 'c_description',)
